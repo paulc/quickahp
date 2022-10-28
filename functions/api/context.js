4650fcc3aa403b8f1643aa53f0535a9008e4fa4f
paulc <212168+paulc@users.noreply.github.com>
2022-10-28 15:43:54 +0100

    Fri 28 Oct 2022 15:43:54 BST

--- functions/api/context.js
+++ functions/api/context.js
@@ -1,5 +1,8 @@
 
 export async function onRequest(context) {
   const data = JSON.stringify(context)
-  return new Response(data, { headers: new Headers({'Access-Control-Allow-Origin':'*', 'Content-Type':'application/json'})} )
+  return new Response(data, { headers: new Headers({
+        'Access-Control-Allow-Origin':'*', 
+        'Content-Type':'application/json'})
+  })
 }
