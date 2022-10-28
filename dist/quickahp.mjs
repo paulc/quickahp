d88b7d5d7fbc18cb35e67224caa028e52846b601
paulc <212168+paulc@users.noreply.github.com>
2022-10-28 15:40:26 +0100

    cors

--- dist/quickahp.mjs
+++ dist/quickahp.mjs
@@ -1,4 +1,8 @@
 
 import { default as Alpine } from "./alpine.js"
 
+Alpine.store('context',
+    await fetch("https://quickahp.pages.dev/api/context").then(r => r.json())
+)
+
 Alpine.start()
--- functions/api/context.js
+++ functions/api/context.js
@@ -1,5 +1,5 @@
 
 export async function onRequest(context) {
-    const data = JSON.stringify(context)
-    return new Response(data)
+  const data = JSON.stringify(context)
+  return new Response(data, { headers: new Headers({'Access-Control-Allow-Origin':'*', 'Content-Type':'application/json'})} )
 }
