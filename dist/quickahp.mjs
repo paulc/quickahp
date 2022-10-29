75de2e255e037f99faa24065a1d4ebcdfe62c2ac
paulc <212168+paulc@users.noreply.github.com>
2022-10-30 00:36:30 +0100

    Field builder

--- dist/quickahp.mjs
+++ dist/quickahp.mjs
@@ -1,8 +1,37 @@
 
 import { default as Alpine } from "./alpine.js"
+import { marked } from "./marked.esm.js"
 
-Alpine.store('context',
-    await fetch("https://quickahp.pages.dev/api/context").then(r => r.json())
-)
+class Field {
+  static index = 0
+  title = ""
+  text = ""
+  constructor() {
+    this.id = Field.index++
+  }
+}
+
+Alpine.data('ahp',() => ({
+
+  context: {},
+  fields: [],
+
+  addField() {
+    this.fields.push(new Field())
+  },
+
+  deleteField(id) {
+    this.fields = this.fields.filter((f) => f.id != id)
+  },
+
+  formatMarkdown(text) {
+    return marked(text, { gfm:true })
+  },
+
+  async init() {
+    this.context = await fetch("https://quickahp.pages.dev/api/context").then(r => r.json())
+  }
+
+}))
 
 Alpine.start()
