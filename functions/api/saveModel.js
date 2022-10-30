d73de0c9d4a08486c9bc458ab590e9659b0be33f
paulc <212168+paulc@users.noreply.github.com>
2022-10-30 22:48:48 +0000

    saveModel.js

--- functions/api/saveModel.js
+++ functions/api/saveModel.js
@@ -2,7 +2,7 @@
 export async function onRequest(context) {
     const { request } = context
     const data = await request.json()
-    const response = JSON.stringify(data)
+    const response = JSON.stringify({ "xxx":111 })
     return new Response(data, { headers: new Headers({
         'Access-Control-Allow-Origin':'*', 
         'Content-Type':'application/json'})
