bde17a9baf6a1000fdaf946c0778bd7ecbabd150
paulc <212168+paulc@users.noreply.github.com>
2022-10-30 23:30:18 +0000

    listKeys.js

--- functions/api/listKeys.js
+++ functions/api/listKeys.js
@@ -2,15 +2,7 @@
 export async function onRequest(context) {
     const { request, env } = context
     try {
-        const { uuid, data } = await request.json()
-        if (uuid === undefined) {
-            throw new Error("Invalid request {uuid}")
-        }
-        if (data === undefined) {
-            throw new Error("Invalid request {data}")
-        }
-        const result = await env.AHP.put(uuid,data)
-        return new Response(JSON.stringify({result: result}), 
+        return new Response(JSON.stringify({keys: await env.AHP.list()}), 
             { headers: new Headers({
                 'Access-Control-Allow-Origin':'*', 
                 'Content-Type':'application/json'})
