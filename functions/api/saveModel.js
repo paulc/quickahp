8d703a2add624249816f54fecc1714354f06bea1
paulc <212168+paulc@users.noreply.github.com>
2022-10-30 22:56:05 +0000

    saveModel.js

--- functions/api/saveModel.js
+++ functions/api/saveModel.js
@@ -1,10 +1,18 @@
 
 export async function onRequest(context) {
     const { request } = context
-    const data = await request.json()
-    return new Response(JSON.stringify(data), 
-        { headers: new Headers({
-            'Access-Control-Allow-Origin':'*', 
-            'Content-Type':'application/json'})
-    })
+    try {
+        const data = await request.json()
+        return new Response(JSON.stringify(data), 
+            { headers: new Headers({
+                'Access-Control-Allow-Origin':'*', 
+                'Content-Type':'application/json'})
+        })
+    } catch(e) {
+        return new Response(JSON.stringify({"error":e}), 
+            { headers: new Headers({
+                'Access-Control-Allow-Origin':'*', 
+                'Content-Type':'application/json'})
+        })
+    }
 }
