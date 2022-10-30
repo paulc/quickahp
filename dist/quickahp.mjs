3305b04a16eeb15d6d550433c6e363259c4c058c
paulc <212168+paulc@users.noreply.github.com>
2022-10-30 18:10:02 +0000

    Model entry form

--- dist/quickahp.mjs
+++ dist/quickahp.mjs
@@ -3,33 +3,52 @@ import { default as Alpine } from "./alpine.js"
 import { marked } from "./marked.esm.js"
 
 class Field {
+
   static index = 0
+
   title = ""
   text = ""
+  preview = false
+
   constructor() {
     this.id = Field.index++
   }
 }
 
+class Model {
+
+  name = ""
+  description = ""
+  criteria = []
+  alternatives = []
+
+  addCriteria() {
+    this.criteria.push(new Field())
+  }
+
+  deleteCriteria(id) {
+    this.criteria = this.criteria.filter((f) => f.id != id)
+  }
+
+  addAlternative() {
+    this.alternatives.push(new Field())
+  }
+
+  deleteAlternative(id) {
+    this.alternatives = this.alternatives.filter((f) => f.id != id)
+  }
+}
+
 Alpine.data('ahp',() => ({
 
-  context: {},
-  fields: [],
-
-  addField() {
-    this.fields.push(new Field())
-  },
-
-  deleteField(id) {
-    this.fields = this.fields.filter((f) => f.id != id)
-  },
+  model: undefined,
 
   formatMarkdown(text) {
     return marked(text, { gfm:true })
   },
 
-  async init() {
-    this.context = await fetch("https://quickahp.pages.dev/api/context").then(r => r.json())
+  init() {
+    this.model = new Model()
   }
 
 }))
